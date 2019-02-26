d101e8039eb92ef459c10542b538e6b8566855c3
lxk1080 <266427651@qq.com>
2019-02-27 00:14:23 +0800

    add menus

--- src/App.vue
+++ src/App.vue
@@ -5,35 +5,12 @@
     </header>
     <div class="main">
       <div class="sider">
-        <Menu class="sider-menu" theme="dark" active-name="1-1" :open-names="['1']">
-          <Submenu name="1">
+        <Menu class="sider-menu" theme="dark" active-name="1-1" :open-names="['1']" accordion>
+          <Submenu v-for="menu in menus" :key="menu.key" :name="menu.key">
             <template slot="title">
-              <Icon type="ios-paper" />内容管理
+              <Icon type="ios-paper" />{{menu.title}}
             </template>
-            <Menu-Item name="1-1">文章管理</Menu-Item>
-            <Menu-Item name="1-2">评论管理</Menu-Item>
-            <Menu-Item name="1-3">举报管理</Menu-Item>
-          </Submenu>
-          <Submenu name="2">
-            <template slot="title">
-              <Icon type="ios-people" />用户管理
-            </template>
-            <Menu-Item name="2-1">新增用户</Menu-Item>
-            <Menu-Item name="2-2">活跃用户</Menu-Item>
-          </Submenu>
-          <Submenu name="3">
-            <template slot="title">
-              <Icon type="ios-stats" />统计分析
-            </template>
-            <MenuGroup title="使用">
-              <Menu-Item name="3-1">新增和启动</Menu-Item>
-              <Menu-Item name="3-2">活跃分析</Menu-Item>
-              <Menu-Item name="3-3">时段分析</Menu-Item>
-            </MenuGroup>
-            <MenuGroup title="留存">
-              <Menu-Item name="3-4">用户留存</Menu-Item>
-              <Menu-Item name="3-5">流失用户</Menu-Item>
-            </MenuGroup>
+            <Menu-Item v-for="subMenu in menu.subMenus" :key="subMenu.key" :name="subMenu.key">{{subMenu.title}}</Menu-Item>
           </Submenu>
         </Menu>
       </div>
@@ -45,12 +22,19 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import menus from 'common/js/menus';
+
   export default {
     name: 'App',
+    data() {
+      return {
+        menus,
+      };
+    },
   };
 </script>
 
-<style scoped lang="stylus" rel="stylesheet/stylus">
+<style lang="stylus" rel="stylesheet/stylus">
   .app
     display flex
     flex-direction column
@@ -65,8 +49,8 @@
       flex 1
       display flex
       .sider
-        flex 0 0 250px
-        width 250px
+        flex 0 0 280px
+        width 280px
         .sider-menu
           height 100%
           width 100% !important
@@ -74,4 +58,8 @@
         flex 1
         border blue solid 1px
         padding 20px
+
+    .ivu-menu-vertical
+      .ivu-menu-submenu-title-icon
+        top 0
 </style>
---
+++ src/common/js/menus.js
@@ -0,0 +1,114 @@
+const menus = [
+  {
+    title: '企业招聘-小程序端_pc端',
+    key: '1',
+    subMenus: [
+      {
+        title: '企业列表-PC端',
+        key: '1-1',
+      },
+      {
+        title: '招聘列表',
+        key: '1-2',
+      },
+      {
+        title: '招聘类型',
+        key: '1-3',
+      },
+    ],
+  },
+  {
+    title: '用户管理-小程序端',
+    key: '2',
+    subMenus: [
+      {
+        title: '用户详情',
+        key: '2-1',
+      },
+    ],
+  },
+  {
+    title: '新闻管理-PC端',
+    key: '3',
+    subMenus: [
+      {
+        title: '新闻列表',
+        key: '3-1',
+      },
+    ],
+  },
+  {
+    title: '广告管理-小程序端+PC端',
+    key: '4',
+    subMenus: [
+      {
+        title: '网站顶部海报-PC端',
+        key: '4-1',
+      },
+      {
+        title: 'banner管理-小程序',
+        key: '4-2',
+      },
+      {
+        title: '首页中部广告-小程序',
+        key: '4-3',
+      },
+      {
+        title: '首页底部广告 小程序',
+        key: '4-4',
+      },
+    ],
+  },
+  {
+    title: '活动管理-小程序端',
+    key: '5',
+    subMenus: [
+      {
+        title: '活动列表',
+        key: '5-1',
+      },
+    ],
+  },
+  {
+    title: '财务管理',
+    key: '6',
+    subMenus: [
+      {
+        title: '订单列表',
+        key: '6-1',
+      },
+      {
+        title: '套餐管理',
+        key: '6-2',
+      },
+      {
+        title: '发票管理',
+        key: '6-3',
+      },
+    ],
+  },
+  {
+    title: '系统设置',
+    key: '7',
+    subMenus: [
+      {
+        title: '数据统计',
+        key: '7-1',
+      },
+      {
+        title: '账号管理',
+        key: '7-2',
+      },
+      {
+        title: '权限管理',
+        key: '7-3',
+      },
+      {
+        title: '操作日志',
+        key: '7-4',
+      },
+    ],
+  },
+];
+
+export default menus;
