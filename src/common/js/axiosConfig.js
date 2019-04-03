2cdb89da1ad9c1cdeb02bdddcea7355a241b4473
lxkee <lixk@shukun.net>
2019-04-03 20:04:54 +0800

    update

--- src/common/js/axiosConfig.js
+++ src/common/js/axiosConfig.js
@@ -1,4 +1,5 @@
 import { axiosInstance } from 'api/common';
+import LoginApis from 'api/loginApis';
 import $store from '../../store';
 import { SET_USER } from '../../store/mutation-types';
 
@@ -6,6 +7,12 @@ const loginCode = '1009';
 
 // 拦截请求
 axiosInstance.interceptors.request.use((req) => {
+  if (!req.url.includes('/adminCheck')) {
+    LoginApis.check().then(res => {
+      console.log(111, res);
+    });
+  }
+
   return req;
 });
 
